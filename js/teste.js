19e3f4cb9dbff9695f14886443374f694e7b3e07
Vini5610 <79315178+Vini5610@users.noreply.github.com>
2022-11-01 15:30:21 -0300

    Create teste.js

---
+++ js/teste.js
@@ -0,0 +1,31 @@
+const btn1 = document.querySelector("#cad-btn2");
+
+btn1.addEventListener("click", changeColor);
+      function changeColor() {
+        window.open("Registro.html");
+}
+
+const btn2 = document.querySelector("#log-btn");
+
+btn2.addEventListener("click", changeColor);
+      function changeColor() {
+        window.open("login.html");
+}
+
+
+const btn3 = document.querySelector("#cad-btn");
+
+btn3.addEventListener("click", changeColor);
+      function changeColor() {
+        window.open("Registro.html");
+}
+
+
+const btn4 = document.querySelector("#loginbtn");
+
+btn4.addEventListener("click", changeColor);
+      function changeColor() {
+        window.open("index.html");
+}
+
+
