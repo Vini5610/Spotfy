6c3b2926de8c87be0e8124cfd4fc0db2d6d6fa4a
Vini5610 <79315178+Vini5610@users.noreply.github.com>
2022-11-01 15:47:03 -0300

    Create data.js

---
+++ sub-paginas/data.js
@@ -0,0 +1,39 @@
+window.audios = [
+  {
+    title:
+      "Fim de semana no Rio",
+    artist: "Teto",
+    cover: "Tetoo.jpg",
+    file: "TetoTT.mpeg"
+  },
+  {
+    title: "felina",
+    artist: "WIU & Ryan SP",
+    cover: "Felina.jpg",
+    file: "Felina.mpeg"
+  },
+  {
+    title: "Dia azul",
+    artist: "Teto",
+    cover: "dia_azul.jfif",
+    file: "dia_azul.mp3"
+  },
+  {
+    title: "Poesia acustica",
+    artist: "Mc Cabelinho,Tz da Coronel,Oruam,L7NNON,Chefin,N.I.N.A, Chris,Xamã,Luisa Sonza",
+    cover: "poesia.jfif",
+    file: "Poesia Acústica13.mp3"
+  },
+  {
+    title: "Vampiro",
+    artist: "Matue",
+    cover: "vampiro.jfif",
+    file: "Vampiro.mp3"
+  },
+  {
+    title: "Carro Forte",
+    artist: " Borges | Orochi | Chefin | Oruam | Bielzin (prod. TKN, Bonk)",
+    cover: "carro_forte.jfif",
+    file: "carro_forte.mp3"
+  }
+];
